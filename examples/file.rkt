add6c7c40a5c622ac9c3848d9b4141ffbd66c42a
nganhkhoa <service.nganhkhoa@gmail.com>
2026-04-29 16:21:10 -0500

    update file.rkt example

--- examples/file.rkt
+++ examples/file.rkt
@@ -2,6 +2,7 @@
 
 (require effect-racket)
 
+(effect do-print (msg))
 (effect remaining-reads ())
 (effect decrease-remaining-reads ())
 (effect do-read ())
@@ -12,29 +13,41 @@
      (with ((file-read-like (cdr data)))
            (continue* (car data)))]))
 
+(define (print-handler)
+  (handler
+    [(do-print msg)
+     (printf "~a~n" msg)
+     (continue null)]))
+
 (define (has-reads-left eff)
   (cond
     [(do-read? eff)
      (> (remaining-reads) 0)]
     [else #true]))
 
-(define (is-eof s)
-  (string=? s "\0"))
+(define (is-eof s) (equal? s 0))
 
 (define (reads-result-correct item)
   (let ([remainings (decrease-remaining-reads)])
-    (cond
-      [(equal? remainings 0) (is-eof item)]
-      [else (string? item)])))
+   (cond
+     [(equal? remainings 0) (is-eof item)]
+     [else (string? item)])))
 
-(define/contract (file-read)
+(define (no-print-allow eff) (not (do-print? eff)))
+
+(define/contract (file-read count)
   (and/c
-    (-> string?)
-    (->e has-reads-left reads-result-correct))
-  (do-read)
-  (do-read)
-  (do-read)
-  (do-read))
+    (-> number? string?)
+    (dependent->e has-reads-left
+                  (match-lambda
+                    [(do-read) reads-result-correct]
+                    [else any/c]))
+    (->e no-print-allow any/c))
+  (string-join (map (λ (_)
+                      (let ([str (do-read)])
+                        ;; (do-print (string-append "reading: " str))
+                        str))
+                    (range count)) "-"))
 
 (define (contract-check n)
   (contract-handler
@@ -47,6 +60,9 @@
      (file-read-like data))
     (f)))
 
-(perform-read-with-data
-  file-read
-  '("a" "b" "c" "\0"))
+(with ((print-handler))
+  (do-print "starts")
+  (perform-read-with-data
+    (λ () (file-read (read)))
+    '("a" "b" "c" 0))
+  (do-print "ends"))
